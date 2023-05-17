2e664cdca988d4940cb04bfe7c6596482b3e2ea8
hankangwen <13538898378@163.com>
2023-05-17 20:34:07 +0800

    完成快速上手。

---
+++ Assets/HotUpdate/Hello.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello
+{
+    public static void Run()
+    {
+        Debug.Log("Hello, World");
+    }
+}
