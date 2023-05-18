c5a5ff713d79aa49834e081b8333a5436b288e51
hankangwen <13538898378@163.com>
2023-05-18 20:26:33 +0800

    新增BuildPlayerCommand

--- Assets/HybridCLRData/Generated/AOTGenericReferences.cs
+++ Assets/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -2,15 +2,22 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 {
 
 	// {{ AOT assemblies
+	// UnityEngine.AssetBundleModule.dll
+	// UnityEngine.CoreModule.dll
+	// mscorlib.dll
 	// }}
 
 	// {{ constraint implement type
 	// }} 
 
 	// {{ AOT generic types
+	// System.Collections.Generic.List<Entry.MyVec3>
 	// }}
 
 	public void RefMethods()
 	{
+		// object UnityEngine.AssetBundle.LoadAsset<object>(string)
+		// object UnityEngine.GameObject.AddComponent<object>()
+		// object UnityEngine.Object.Instantiate<object>(object)
 	}
 }
